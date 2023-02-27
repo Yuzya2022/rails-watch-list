c782fc3d7023a11bfec2772c11f83d465c84fe86
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-02-27 17:04:32 +0200

    Routing, Controller, Views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "lists#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists do
+    # * Nested routes with relation to reviews -> /restaurants/:restaurants_id/new
+    resources :bookmarks, only: %i[index new show create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
