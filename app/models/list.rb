097a20e434584f40a9699c0e4c63f91ce75b78e7
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-03-03 12:21:19 +0200

    Front-end updated

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   has_many :bookmarks
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_one_attached :photo
 end
